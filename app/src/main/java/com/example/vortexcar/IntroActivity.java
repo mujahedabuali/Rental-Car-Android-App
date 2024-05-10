1e238b6c2fbfc5359854c60061545e6c9583d363
mujahedabuali <mujahedabuali0@gmail.com>
2024-05-10 11:33:31 +0300

    Vortexxx

---
+++ app/src/main/java/com/example/vortexcar/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.vortexcar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intro);
+
+    }
+
+    public void start(View view) {
+        Intent intent = new Intent(this, loginActivity.class);
+        startActivity(intent);
+    }
+
+}
