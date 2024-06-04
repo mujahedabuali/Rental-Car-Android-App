6ff449d31056fee782af846d3a1e649bf8055297
Mujahed Abuali <150655313+mujahedabuali@users.noreply.github.com>
2024-06-04 21:52:20 +0300

    Add files via upload

---
+++ rental-car/getAllCars.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'dbconfig.in.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $stmt = $pdo->query('SELECT * FROM car');
+    $cars = $stmt->fetchAll();
+    header('Content-Type: application/json');
+    echo json_encode($cars);
+} else {
+    header('HTTP/1.1 405 Method Not Allowed');
+    echo json_encode(['error' => 'Only GET method is allowed']);
+}
+?>
