9b8b07ca2b3386717a57672f63dddd17f0af376c
Mujahed Abuali <150655313+mujahedabuali@users.noreply.github.com>
2024-06-06 20:57:36 +0300

    Update register.php

--- rental-car/register.php
+++ rental-car/register.php
@@ -7,39 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $password = $_POST["password"];
     $gender = $_POST["gender"];
     $phone = $_POST["phone"];
+    $type = $_POST["type"];
     
     $errors = [];
     
-    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Invalid email.";
-    }
-    if (empty($name)) {
-        $errors[] = "Name is required.";
-    }
-    if (empty($password)) {
-        $errors[] = "Password is required.";
-    } else {
-        if (strlen($password) < 8) {
-            $errors[] = "Password must be at least 8 characters long.";
-        }
-        if (!preg_match('/[A-Za-z]/', $password)) {
-            $errors[] = "Password must contain at least one letter.";
-        }
-        if (!preg_match('/[0-9]/', $password)) {
-            $errors[] = "Password must contain at least one number.";
-        }
-        if (!preg_match('/[!@#$%^&*(),.?":{}|<>]/', $password)) {
-            $errors[] = "Password must contain at least one special character.";
-        }
-    }
-    if (empty($gender)) {
-        $errors[] = "Gender is required.";
-    }
-    if (empty($phone)) {
-        $errors[] = "Phone number is required.";
-    }
     try {
-        $checkEmailSql = "SELECT COUNT(*) FROM Account WHERE email = :email";
+        $checkEmailSql = "SELECT COUNT(*) FROM account WHERE email = :email";
         $checkEmailStmt = $pdo->prepare($checkEmailSql);
         $checkEmailStmt->bindValue(':email', $email);
         $checkEmailStmt->execute();
@@ -53,15 +26,16 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
             echo json_encode(["status" => "error", "messages" => $errors]);
             exit();
         }
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO Account (email, name, password, gender, phone) VALUES (:email, :name, :password, :gender, :phone)";
+        $sql = "INSERT INTO account (name, Type, Phone, password, email,gender) VALUES (:name, :type, :phone, :pass, :email,:gender)";
         $stmt = $pdo->prepare($sql);
         
-        $stmt->bindValue(':email', $email);
         $stmt->bindValue(':name', $name);
-        $stmt->bindValue(':password', $hashedPassword);
-        $stmt->bindValue(':gender', $gender);
+        $stmt->bindValue(':type', $type);
         $stmt->bindValue(':phone', $phone);
+        $stmt->bindValue(':pass', $password);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':gender', $gender);
+        
 
         if ($stmt->execute()) {
             echo json_encode(["status" => "success", "message" => "User registered successfully."]);
