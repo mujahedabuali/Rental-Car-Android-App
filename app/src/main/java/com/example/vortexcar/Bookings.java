08e7eabd0685095c4b056383f6e8880e71507978
mujahedabuali <mujahedabuali0@gmail.com>
2024-06-10 22:51:42 +0300

    Vortexxx

--- app/src/main/java/com/example/vortexcar/Bookings.java
+++ app/src/main/java/com/example/vortexcar/Bookings.java
@@ -3,6 +3,7 @@ package com.example.vortexcar;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -41,12 +42,16 @@ public class Bookings extends AppCompatActivity {
     private TextView rateEDT;
     private TextView daysNumberEDT;
     private TextView totalEDT;
+    private TextView pickup;
 
     private TextView nametv;
     private TextView emailtv;
     private TextView phoneNumbertv;
     private Button confirm;
     private String name;
+
+    private String carID;
+    private String accID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,15 +66,18 @@ public class Bookings extends AppCompatActivity {
         totalEDT = findViewById(R.id.totalCost);
 
         confirm = findViewById(R.id.confirm);
+        pickup = findViewById(R.id.pickup);
 
         nametv = findViewById(R.id.name);
         emailtv = findViewById(R.id.email);
         phoneNumbertv = findViewById(R.id.phoneNumber);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
-        name = sharedPreferences.getString("name", "Guest");
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        name = sharedPreferences.getString("first_name", "Guest");
+        name = name+" "+ sharedPreferences.getString("last_name","");
         String email = sharedPreferences.getString("email", "noemail");
-        String phone = sharedPreferences.getString("phone", "**********");
+        String phone = sharedPreferences.getString("phone", "nophone");
+        accID = sharedPreferences.getString("accID","1");
 
         this.nametv.setText(name);
         this.emailtv.setText(email);
@@ -83,20 +91,34 @@ public class Bookings extends AppCompatActivity {
         double rate = intent.getDoubleExtra("rate", 0);
         double daysNumber = intent.getDoubleExtra("daysNumber", 0);
         double total = intent.getDoubleExtra("total", 0);
+        carID = intent.getStringExtra("id");
+        boolean isMonthly = intent.getBooleanExtra("isMonthly",false);
+
+
 
         // Populate the EditText fields with the retrieved data
         carInfoEDT.setText(carInfo);
         dropoffEDT.setText(dropoff);
-        rateEDT.setText(String.valueOf(rate) + "$/day");
+        rateEDT.setText(String.valueOf(rate) + (isMonthly ? " $/Month": "$/Day"));
         daysNumberEDT.setText(String.valueOf(daysNumber));
-        totalEDT.setText(String.valueOf(total) + "$");
+        totalEDT.setText(String.valueOf(total) + " $");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            LocalDate today = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            String formattedDate = today.format(formatter);
+            pickup.setText(formattedDate);
+        }
 
         confirm.setOnClickListener(e -> {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
                 LocalDate today = LocalDate.now();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
                 String formattedDate = today.format(formatter);
-                rent(sharedPreferences, total, formattedDate, dropoff);
+                if (!email.equals("noemail") && !phone.equals("nophone"))
+                    rent(sharedPreferences, total, formattedDate, dropoff);
+                else
+                    Toast.makeText(Bookings.this, "Please log in to complete the booking.", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -104,13 +126,13 @@ public class Bookings extends AppCompatActivity {
     private void rent(SharedPreferences data, double totalPrice, String startDate, String endDate) {
         if (!this.name.equals("Guest")) {
             RequestQueue queue = Volley.newRequestQueue(this);
-            String url = "https://192.168.56.1/rentalcar/makeRent.php"; // replace with your server URL
+            String url = vars.BASE_URL + "/rental-car/makeRent.php";
 
             StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                     new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
-                            // Handle response from the server
+                            Log.d("ServerResponse", response);
                             try {
                                 JSONObject jsonResponse = new JSONObject(response);
                                 boolean success = jsonResponse.getBoolean("success");
@@ -118,6 +140,8 @@ public class Bookings extends AppCompatActivity {
 
                                 if (success) {
                                     Toast.makeText(Bookings.this, "Booking successful: " + message, Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Bookings.this, homePage.class);
+                                    startActivity(intent);
                                 } else {
                                     Toast.makeText(Bookings.this, "Booking failed: " + message, Toast.LENGTH_LONG).show();
                                 }
@@ -129,19 +153,18 @@ public class Bookings extends AppCompatActivity {
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    // Handle error
                     Toast.makeText(Bookings.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }) {
                 @Override
                 protected Map<String, String> getParams() {
                     Map<String, String> params = new HashMap<>();
-                    params.put("carID", "1"); // Replace with actual carID
-                    params.put("accountID", "1"); // Replace with actual accountID
+                    params.put("carID", carID);
+                    params.put("accountID", accID);
                     params.put("totalPrice", String.valueOf(totalPrice));
-                    params.put("status", "confirmed"); // or any other status
-                    params.put("startDate", startDate);
-                    params.put("endDate", endDate);
+                    params.put("status", "confirmed");
+                    params.put("startDate", Helper.convertDateFormat(startDate));
+                    params.put("endDate", Helper.convertDateFormat(endDate));
                     return params;
                 }
             };
@@ -151,7 +174,14 @@ public class Bookings extends AppCompatActivity {
             Toast.makeText(Bookings.this, "Please log in to complete the booking.", Toast.LENGTH_LONG).show();
         }
     }
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
 }
-//TODO: back arrow into main activity
-// +get customer & the car that he booked info
-// +return car button
+
